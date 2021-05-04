69d0a19ce7c6a21db9981d58a41b12359acea0c3
jafetimbre <jafet.imbre@gmail.com>
2021-05-04 17:10:48 +0300

    depl

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('asd')
+
+$('#sortable-list').sortable({
+	// SortableJS options go here
+	// See: (https://github.com/SortableJS/Sortable#options)
+
+	handle: '.handle',
+	invertSwap: true,
+    animation: 100
+	// . . .
+})
